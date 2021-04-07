878d5525a05df9eaba1af72b84d857ddabab6d39
zoechen <iki0723@gmail.com>
2021-04-07 16:29:26 +0800

    s001

--- JSON_generator.dart
+++ JSON_generator.dart
@@ -1,4 +1,4 @@
-[
+S002:[
  '{{repeat(78, 97)}}',
   {
     id: '{{index()}}',
@@ -18,6 +18,28 @@
         return '223400201000'+i;
       }else{
         return '22340020100'+i;
+      }
+    },
+    row_code: '{{random("A","B","C","D","E")}}'
+  } 
+],
+[
+ '{{repeat(12, 20)}}',
+  {
+    id: '{{index()+89}}',
+    eartag: '{{floating(100000,1000000,0,"000000")}}',
+    isActive: '{{bool()}}',
+    swine_type:'S001',
+    state_code:"01103",
+    estrus_code: '99',
+    post_code:"223400",
+    zone_code:"22340020",
+    house_code:"2234002010",
+    pen_code:function(t,i){
+      if((i+89) < 100){
+        return '22340020100'+i+89;
+      }else{
+        return '2234002010'+i+89;
       }
     },
     row_code: '{{random("A","B","C","D","E")}}'
